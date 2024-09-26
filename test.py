30b0bf2aee2761865ea495719ffb6e4cc7347d19
Ali <ali.zaringhalam@gmail.com>
2024-09-26 15:22:23 -0400

    added an argparse

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 
 land_cover_classes = [0, 1, 5, 60, 100]
 
@@ -11,4 +11,7 @@ def f(path):
     print(f'{unique_counts} of the desired landcover classes are in {path}')
 
 if __name__ == '__main__':
-    f(sys.argv[1])
+    p = argparse.ArgumentParser()
+    p.add_argument("--file_name", "-f",  help="input file name to be line counted")
+    args = p.parse_args()
+    f(args.file_name)
